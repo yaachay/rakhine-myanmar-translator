343d4bd2032edd40ab1e567e5c109136f15255a1
yaachay <yachay498@gmail.com>
2024-07-24 04:59:31 +0630

    All function are done! TODO: It will be better with Splash Screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,12 @@
 package application;
-	
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Timer;
+import java.util.TimerTask;
 
-import javax.swing.plaf.synth.SynthStyleFactory;
-
-import application.server.TranslatorService;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
@@ -13,13 +14,21 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 
-
 public class Main extends Application {
+	Parent root;
 	Image icon = new Image(getClass().getResourceAsStream("icon.png"));
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Registry localRegistry = LocateRegistry.getRegistry("127.0.01", 1200);
+			boolean registryAccessible = isRegistryAccessible(localRegistry, "TranslatorService");
+
+			if (registryAccessible) {
+				root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			} else {
+				root = FXMLLoader.load(getClass().getResource("Error.fxml"));
+			}
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.getIcons().add(icon);
@@ -27,23 +36,23 @@ public class Main extends Application {
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
 			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public static void main(String[] args) {
-		String rkTitle = "ဇာကိုလားလေ့။";
-		
-		try
-		{
-			Registry registry = LocateRegistry.getRegistry("127.0.01", 1200);
-			TranslatorService translatorService = (TranslatorService) registry.lookup("TranslatorService");
-			System.out.print(translatorService.translateRK2MY(rkTitle));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+	}
+
+	public static boolean isRegistryAccessible(Registry registry, String remoteObjectName) {
+		try {
+			// Attempt to lookup the specified remote object in the registry
+			registry.lookup(remoteObjectName);
+			return true; // Lookup successful, registry is accessible
+		} catch (RemoteException | NotBoundException e) {
+			// Lookup failed, registry might not be accessible or object not found
+			return false;
+		}
+	}
+
+	public static void main(String[] args) {
 		launch(args);
 	}
 }
